d2cba6600d4725a92508f4fc426bf8ad47281a38
Jiahao, Woo <woojiahao1234@gmail.com>
2026-06-29 08:55:56 +0800

    Add StagingArea widget

---
+++ components/widgets/staging_area.py
@@ -0,0 +1,32 @@
+from pathlib import Path
+from typing import Set
+
+from components.file_option import FileOption
+from textual.app import ComposeResult
+from textual.containers import Vertical
+from textual.reactive import reactive
+from textual.widget import Widget
+from textual.widgets import Label, OptionList
+from textual.widgets.option_list import Option
+
+
+class StagingArea(Widget):
+    selected_folders: reactive[Set[Path]] = reactive(set)
+
+    def __init__(self) -> None:
+        super().__init__()
+
+    def compose(self) -> ComposeResult:
+        with Vertical(id="staging-area"):
+            yield Label("Staged", classes="pane-header")
+            yield OptionList(id="staging-list")
+
+    def watch_selected_folders(self, value: Set[Path]) -> None:
+        staging_area = self.query_one("#staging-list", OptionList)
+
+        if not value:
+            staging_area.add_option(Option("[No Folders Staged]"))
+            return
+
+        for folder_path in sorted(value):
+            staging_area.add_option(FileOption(folder_path, True))
